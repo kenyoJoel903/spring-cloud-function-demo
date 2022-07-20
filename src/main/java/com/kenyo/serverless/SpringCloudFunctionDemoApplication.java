560ab0cf76486d3285471a2d86a87dec8b4903e7
Kenyo Joel Pecho Ñaupari <kenyojoel903@gmail.com>
2022-07-20 07:40:14 -0500

    inicio

---
+++ src/main/java/com/kenyo/serverless/SpringCloudFunctionDemoApplication.java
@@ -0,0 +1,13 @@
+package com.kenyo.serverless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudFunctionDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudFunctionDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kenyo/serverless/SpringCloudFunctionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kenyo.serverless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudFunctionDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
